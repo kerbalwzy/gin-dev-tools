04d155ef2c1110a5941fcf973e3887c00e871fd0
kerbalwzy <kerbalwzy@gmail.com>
2019-11-11 23:49:38 +0800

    Update:CORSMiddleware

---
+++ middlewares/CORSHeaders.go
@@ -0,0 +1,24 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Add CORS headers in response message.
+// Allow any origin and take the cookie data here, You may need change for your own demands.
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+
+	}
+}
