d37f5239362c18a645407a526f9bda796ddf2a28
kerbalwzy <kerbalwzy@gmail.com>
2021-05-07 17:24:30 +0800

    Update: 重新整理项目

---
+++ u_jwt_test.go
@@ -0,0 +1,38 @@
+package kerbalwzygo
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"testing"
+	"time"
+)
+
+func TestJWTToken(t *testing.T) {
+	claims := &CustomJWTClaims{
+		CustomData: "hello world",
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: NowTimestamp() + 5,
+		},
+	}
+	res, err := CreateJWTToken(*claims, []byte("Testing"))
+	if nil != err {
+		t.Fatal(err)
+	} else {
+		t.Log(res)
+	}
+
+	claims, err = ParseJWTToken(res, []byte("Testing"))
+	if nil != err {
+		t.Fatal(err)
+	} else {
+		t.Log(*claims)
+	}
+
+	res, err = RefreshJWTToken(res, []byte("Testing"), 100*time.Second)
+	if nil != err {
+		t.Fatal(err)
+	} else {
+		t.Log(res)
+		claims, _ = ParseJWTToken(res, []byte("Testing"))
+		t.Log(*claims)
+	}
+}
